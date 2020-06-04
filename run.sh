1508c4ae013793002cdb580bee2ae9b8c18264d5
Your Name <you@example.com>
2020-06-03 21:44:16 -0300

    run update

--- run.sh
+++ run.sh
@@ -5,10 +5,7 @@ set -e
 #uncomment to a more verbose script
 #set -x
 
-FAULTS=10
-cfc=carol-fi-codes
-
-declare -A benchmarks=( 
+declare -A BENCHMARKS=( 
         ["lava_mp"]="${cfc}/lava_mp/float_dmr_none.conf"
         ["gemm_tensorcores"]="${cfc}/gemm_tensorcores/single_mxm_no_tensor.conf"
         ["bfs"]="${cfc}/bfs/bfs.conf"
@@ -20,28 +17,121 @@ declare -A benchmarks=(
         ["lud"]="${cfc}/lud/lud.conf"
         ["nw"]="${cfc}/nw/nw.conf"
         )
-          
-cd ../
-
-for bench in "${!benchmarks[@]}";
-do
-    #bench=${benchmarks[$i]}
-    conf="${benchmarks[$bench]}"
-
-    echo "Step 1 - Profiling $bench for fault injection"
-    ./app_profiler.py -c ${conf}
 
 
-    echo "Step 2 - Running ${FAULTS} on ${conf}"
-    #./fault_injector.py -i ${FAULTS} -c ${conf}
+install(){
+    mkdir -p data
 
-    #tar czf ${bench}_carolfi_2k.tar.gz *.csv /var/radiation-benchmarks/log/
-    #rm -rf /var/radiation-benchmarks/log/*.log logs/* *.csv
-done
+    for bench in "${!BENCHMARKS[@]}";
+    do
+        mkdir -p data/${bench}
+    done
 
-cd -
+    # Download the input data
+    echo "Downloading"
 
-echo "Fault injection finished"
+    # BFS
+    cd data/bfs
+    FILE=graphs_rodinia.tar.gz
+    if [ -f "$FILE" ]; then
+        echo "$FILE exist"
+    else 
+        wget https://github.com/UFRGS-CAROL/radiation-benchmarks/raw/master/data/bfs/${FILE}
+    fi
+    tar xzf $FILE
+    cd -
 
+    # accl
+    cd data/accl
+    FILE=7Frames.pgm
+    if [ -f "$FILE" ]; then
+        echo "$FILE exist"
+    else 
+        wget https://github.com/UFRGS-CAROL/radiation-benchmarks/raw/master/data/accl/7Frames.pgm
+    fi
+
+    cd -
+
+
+    # hotspot
+    cd data/hotspot
+    FILE=power_and_temp_files.tar.gz
+    if [ -f "$FILE" ]; then
+        echo "$FILE exist"
+    else 
+        wget https://github.com/UFRGS-CAROL/radiation-benchmarks/raw/master/data/hotspot/power_and_temp_files.tar.gz
+    fi
+    tar xzf $FILE
+
+    cd -
+
+    make -C common/include/
+    make -C common/include/log_helper_swig_wraper/
+
+    for bench in "${!BENCHMARKS[@]}";
+    do
+        make -C ${bench} -j4
+        make -C ${bench} generate
+    done
+}
+
+clean(){
+        
+    for bench in "${!BENCHMARKS[@]}";
+    do
+        rm -rf data/${bench}
+        make -C ${bench}/ clean
+    done
+
+    rm -rf data
+
+    make -C common/include/ clean
+    make -C common/include/log_helper_swig_wraper/ clean
+}
+
+inject_faults() {
+    
+    FAULTS=10
+    cfc=carol-fi-codes
+            
+    cd ../
+
+    for bench in "${!BENCHMARKS[@]}";
+    do
+        #bench=${benchmarks[$i]}
+        conf="${BENCHMARKS[$bench]}"
+
+        echo "Step 1 - Profiling $bench for fault injection"
+        ./app_profiler.py -c ${conf}
+
+
+        echo "Step 2 - Running ${FAULTS} on ${conf}"
+        #./fault_injector.py -i ${FAULTS} -c ${conf}
+
+        #tar czf ${bench}_carolfi_2k.tar.gz *.csv /var/radiation-benchmarks/log/
+        #rm -rf /var/radiation-benchmarks/log/*.log logs/* *.csv
+    done
+
+    cd -
+
+    echo "Fault injection finished"
+}
+
+
+case $1 in
+    "clean") 
+        clean
+        ;;
+    "install")
+        install
+        ;;
+    "") 
+        inject_faults
+        ;;
+    *) 
+        echo "Invalid option" 
+        ;;
+
+esac
 
 exit 0
